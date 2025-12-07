35816c4caaa96a5a1054f2ecf36be796d3bcb2f2
VishyDhanu <vdhanushan.edu@gmail.com>
2025-12-07 17:11:56 +0530

    Hero component created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { resume } from "react-dom/server";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,10 +17,18 @@ export default function Home() {
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Stay Ahead with AI-Powered Tracking</h1>
           <h2>AI-Powered Reviews Made Simple</h2>
         </div>
+
+        {resume.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
